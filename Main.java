a7fda1d3543fd7b6b0345f4007676b856c3d5e23
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 15:48:28 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Will take a user number and use it in the hailstone sequence
+ * @author Cole Girling
  */
 public class Main {
 
@@ -9,7 +11,32 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //ask the user for a positive integer
+    System.out.println("Please enter a positive integer to see the hailstone sequence.");
+    int n = input.nextInt();
+
+    //Displays a message before the sequence 
+    System.out.println("Hailstone sequence is the following:");
+   
+    //Displays the user number before the sequence
+    System.out.println(n);
+
+    //while n is not equal to one this loop runs
+    while(n != 1){
+      //if n is even will divide it by 2
+      if(n % 2 == 0){
+       n = n / 2;
+       //displays n
+       System.out.println(n);
+      //if n is odd will multiply by 3 and add 1
+      }else if(n % 2 == 1){
+        n = 3 * n + 1;
+        //displays n
+        System.out.println(n);
+      }
+    }
   }
 }
